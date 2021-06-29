5e673a44030894e584f2d0f9b2712812b82be8a6
ahmedmansour <a.a2051995@gmail.com>
2021-06-29 03:51:33 +0200

    test the new methods like spark dataframe

--- src/main/java/com/test/main.java
+++ src/main/java/com/test/main.java
@@ -1,13 +1,14 @@
-
+package com.test;
 import java.util.List;
 
 
 public class main {
     public static void main(String[] args) {
         //Get Data
-        JobDaoImp _JobDaoImp = new JobDaoImp("Wuzzuf_Jobs.csv");
-        List<Job> jobLst = (List<Job>)_JobDaoImp.GetAllJobs();
+        JobDaoImp _JobDaoImp = new JobDaoImp();
+        List<Job> jobLst = _JobDaoImp.ReadFromCSV("Wuzzuf_Jobs.csv");
         System.out.println("************ This CSV file contains top  = " + jobLst.size()+ " Jobs ***************");
+        _JobDaoImp.readCsv("Wuzzuf_Jobs.csv");
 
     }
  
